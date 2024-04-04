1f0c9a6d1e5864a82c233677c2a8eb333c37fbee
Tayde M. Cruz <tayde.martin@gmail.com>
2024-04-04 15:25:40 -0600

    Test 04042024

--- Build/GaleriasVirtualesPrototipo.loader.js
+++ Build/GaleriasVirtualesPrototipo.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 1,
     },
-    wasmFileSize: 92274144,
+    wasmFileSize: 92274405,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
